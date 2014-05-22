eaf2e34caa0c679414c99167600d2dfe4f165993
c13iscuit <barnett.chri@gmail.com>
2014-05-22 07:49:01 -0400

    Fully functional - needs styling

--- server.rb
+++ server.rb
@@ -2,19 +2,19 @@ require 'sinatra'
 require 'csv'
 
   def build_films(csvfile)
-    @films_hash = {}
+    @films_array = []
     CSV.foreach(csvfile, headers: true) do |row|
       film_hash = {}
       film_hash[:movie_id] = row["id"]
-      # film_hash[:title] = row["title"]
+      film_hash[:title] = row["title"]
       film_hash[:year] = row["year"]
       film_hash[:syn] = row["synopsis"]
       film_hash[:rating] = row["rating"]
       film_hash[:genre] = row["genre"]
       film_hash[:studio] = row["studio"]
-      @films_hash[row["title"]] = film_hash
+      @films_array << film_hash
     end
-  @films_hash = (@films_hash.sort_by {|key, value| key})
+  @films_array = (@films_array.sort_by {|key, value| key[:title]})
   puts "hello"
   end
 
@@ -24,7 +24,7 @@ get "/movies" do
 end
 
 get "/movies/:movie_id" do
-  puts "goodbye"
   build_films("movies.csv")
-  erb :views
+  erb :index
 end
+
